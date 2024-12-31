82b8cc9da640660164b57e4ded1a83847d2d6746
Julianafrego <julianafgomes@aluno.uespi.br>
2024-12-30 21:53:16 -0300

    adicionar cores do projeto

---
+++ lib/ui/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Color(0xFFF1FFF4);
+const surfaceColor = Color(0xFFFEFFFE);
+
+const accentPrimary = Color(0xFF29785A);
+const accentSecondary = Color(0xFF48A280);
+
+const highlightNeutral = Color(0xFFE9FFD8);
+const highlightSoft = Color(0xFF6E9589);
