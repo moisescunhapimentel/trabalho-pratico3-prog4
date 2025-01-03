8b8c077bb743b28f2150d92a8894a5ba9cb3eb34
Julianafrego <julianafgomes@aluno.uespi.br>
2025-01-03 14:36:36 -0300

    adiconar appbar e botao flutuante em ContratosScreen

--- lib/ui/screens/contratos_screen.dart
+++ lib/ui/screens/contratos_screen.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:trabalho3/ui/widgets/imovel_item_button.dart';
 import '../../data/enums/tipo_intervalo.dart';
-
+import 'package:trabalho3/ui/theme/app_theme.dart';
+import 'package:trabalho3/ui/widgets/icone_flutuante_button.dart';
 class ContratosScreen extends StatelessWidget {
   const ContratosScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contratos ativos', ),
+      ),
+      body: Container(
       padding: const EdgeInsets.all(10),
       child: Column(children: [
-        const Text('Vencimento Próximo'),
         ImovelItemButton(
           nome: 'Apartamento em Camocim',
           parcelas: '3',
@@ -19,6 +23,12 @@ class ContratosScreen extends StatelessWidget {
           dataVencimento: DateTime(2024, 12, 31),
         ),
       ]),
+      
+    ),
+      floatingActionButton: IconeFlutuanteButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
+     
+    
   }
 }
