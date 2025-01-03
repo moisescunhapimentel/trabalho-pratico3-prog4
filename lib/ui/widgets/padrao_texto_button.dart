5548ceb2a71294ec65ef3bc4bb7bcb276f1ea80b
Julianafrego <julianafgomes@aluno.uespi.br>
2025-01-03 14:44:34 -0300

    adicionar PadraoTextoButton

---
+++ lib/ui/widgets/padrao_texto_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:trabalho3/ui/constants/colors.dart';
+
+class PadraoTextoButton extends StatelessWidget {
+  final String texto;
+  final void Function() onPressed;
+  
+  const PadraoTextoButton({super.key, required this.texto, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return TextButton(
+      onPressed: () {},
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(accentSecondary),
+        padding: MaterialStateProperty.all(const EdgeInsets.symmetric(vertical: 16, horizontal: 30)),
+        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(6),
+        )),
+      ),
+      child: Text(
+        texto,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+}
