412bd135324fb4a4fef2373334188884913eacb1
Julianafrego <julianafgomes@aluno.uespi.br>
2025-01-03 14:53:53 -0300

    alterar TextoIconeButton para receber valores e alterações de estilo

--- lib/ui/widgets/texto_icone_button.dart
+++ lib/ui/widgets/texto_icone_button.dart
@@ -1,32 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:trabalho3/ui/constants/colors.dart';  
+import 'package:trabalho3/ui/constants/colors.dart';
 
 class TextoIconeButton extends StatelessWidget {
   final String texto;
   final IconData icone;
   final Function funcao;
-  const TextoIconeButton({super.key, required this.texto, required this.icone, required this.funcao});
+  const TextoIconeButton(
+      {super.key,
+      required this.texto,
+      required this.icone,
+      required this.funcao});
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {funcao();},
-      style: ElevatedButton.styleFrom(
+    var larguraScreen = MediaQuery.of(context).size.width;
 
-        backgroundColor: backgroundColor,
-        padding: const EdgeInsets.all(20), 
+    return ElevatedButton(
+      onPressed: () {
+        funcao();
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: surfaceColor,
+        padding: EdgeInsets.symmetric(vertical: (larguraScreen * 0.05), horizontal: (larguraScreen * 0.01)),
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10), 
+          borderRadius: BorderRadius.circular(10),
         ),
-        fixedSize: const Size(150, 150), 
-         elevation: 8,
+        fixedSize: const Size(210, 150),
+        elevation: 8,
       ),
       child: Column(
-        mainAxisSize: MainAxisSize.min, 
+        mainAxisSize: MainAxisSize.min,
         children: [
-          Icon(icone, color: highlightSoft, size: 50), 
+          Icon(icone, color: highlightSoft, size: 50),
           const SizedBox(height: 5),
-          Text(texto, style: const TextStyle(color: highlightSoft, fontSize: 20)), 
+          Text(texto,
+              style: const TextStyle(color: highlightSoft, fontSize: 20)),
         ],
       ),
     );
