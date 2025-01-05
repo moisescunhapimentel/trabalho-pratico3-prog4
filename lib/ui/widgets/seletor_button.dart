36c6d865d6fe4b0633f578651e07205011ce3dc1
Julianafrego <julianafgomes@aluno.uespi.br>
2025-01-05 16:22:20 -0300

    atualizar SeletorButton

--- lib/ui/widgets/seletor_button.dart
+++ lib/ui/widgets/seletor_button.dart
@@ -5,16 +5,39 @@ import 'package:trabalho3/ui/widgets/padrao_texto_button.dart';
 class SeletorButton extends StatefulWidget {
   final String opcao1;
   final String opcao2;
+  final String selectedOption;
   final Function(String) funcao;
-  
-  const SeletorButton({super.key, required this.opcao1, required this.opcao2, required this.funcao});
+
+  const SeletorButton({
+    super.key,
+    required this.opcao1,
+    required this.opcao2,
+    required this.selectedOption,
+    required this.funcao,
+  });
 
   @override
   _SeletorButtonState createState() => _SeletorButtonState();
 }
 
 class _SeletorButtonState extends State<SeletorButton> {
-  String? _opcaoSelecionada;
+  late String _opcaoSelecionada;
+
+  @override
+  void initState() {
+    super.initState();
+    _opcaoSelecionada = widget.selectedOption;
+  }
+
+  @override
+  void didUpdateWidget(covariant SeletorButton oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.selectedOption != oldWidget.selectedOption) {
+      setState(() {
+        _opcaoSelecionada = widget.selectedOption;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,36 +46,36 @@ class _SeletorButtonState extends State<SeletorButton> {
     final Color surfaceColor = theme.colorScheme.surface;
 
     return Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          PadraoTextoButton(
-            texto: widget.opcao1,
-            cor: _opcaoSelecionada == widget.opcao1 ? backgroundColor : Colors.black,
-            corFundo: _opcaoSelecionada == widget.opcao1
-                ? accentPrimary
-                : surfaceColor,
-            funcao: () {
-              setState(() {
-                _opcaoSelecionada = widget.opcao1;
-              });
-              widget.funcao(widget.opcao1); 
-            },
-          ),
-          PadraoTextoButton(
-            texto: widget.opcao2,
-            cor: _opcaoSelecionada == widget.opcao2 ? backgroundColor : Colors.black,
-            corFundo: _opcaoSelecionada == widget.opcao2
-                ? accentSecondary
-                : surfaceColor,
-            funcao: () {
-              setState(() {
-                _opcaoSelecionada = widget.opcao2;
-              });
-              widget.funcao(widget.opcao2); 
-
-            },
-          ),
-        ], 
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        PadraoTextoButton(
+          texto: widget.opcao1,
+          cor: _opcaoSelecionada == widget.opcao1 ? backgroundColor : Colors.black,
+          corFundo: _opcaoSelecionada == widget.opcao1
+              ? accentPrimary
+              : surfaceColor,
+          funcao: () {
+            setState(() {
+              _opcaoSelecionada = widget.opcao1;
+            });
+            widget.funcao(widget.opcao1);
+          },
+        ),
+        const SizedBox(width: 10),
+        PadraoTextoButton(
+          texto: widget.opcao2,
+          cor: _opcaoSelecionada == widget.opcao2 ? backgroundColor : Colors.black,
+          corFundo: _opcaoSelecionada == widget.opcao2
+              ? accentPrimary
+              : surfaceColor,
+          funcao: () {
+            setState(() {
+              _opcaoSelecionada = widget.opcao2;
+            });
+            widget.funcao(widget.opcao2);
+          },
+        ),
+      ],
     );
   }
 }
