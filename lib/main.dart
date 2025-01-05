105a71459e608b198fcd3aadce64c73b1caddf55
Julianafrego <julianafgomes@aluno.uespi.br>
2025-01-05 18:21:25 -0300

    adicionar providers em main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:trabalho3/ui/models/imovel_model.dart';
 import 'package:trabalho3/ui/screens/inicio_screen.dart';
 import 'package:trabalho3/ui/theme/app_theme.dart';
 import 'package:trabalho3/ui/widgets/barra_navegação_widget.dart';
-import 'package:trabalho3/data/models/navegacao_item.dart';
+import 'package:trabalho3/ui/models/navegacao_item_model.dart';
+import 'package:trabalho3/ui/models/formulario_imovel.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ImovelModel()),
+        ChangeNotifierProvider(create: (context) => FormularioImovelModel()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
+
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
   @override
