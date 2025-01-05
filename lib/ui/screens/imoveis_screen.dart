89c55024eca32afa573e5e413c2337807c67bc0f
Julianafrego <julianafgomes@aluno.uespi.br>
2025-01-05 18:24:02 -0300

    adicionar rota a floatingButton em ImoveisScreen

--- lib/ui/screens/imoveis_screen.dart
+++ lib/ui/screens/imoveis_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:trabalho3/ui/widgets/icone_flutuante_button.dart';
+import 'package:trabalho3/ui/widgets/imovel_item_card.dart';
+import 'package:trabalho3/ui/screens/formulario_imovel_screen.dart';
 
 class ImoveisScreen extends StatelessWidget {
   const ImoveisScreen({super.key});
@@ -7,13 +9,40 @@ class ImoveisScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Imóveis'),
-        ),
-        body: const Column(
-        ),
-        floatingActionButton: IconeFlutuanteButton(),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      );
+      appBar: AppBar(
+        title: const Text('Imóveis'),
+      ),
+      body: ListView(
+        children: [
+          ImovelItemCard(
+            nomeImovel: 'Casa em João Pessoa',
+            rua: 'Rua do alegria',
+            numImovel: '123',
+            numComodos: 15,
+          ),
+          ImovelItemCard(
+            nomeImovel: 'Casa em João Pessoa',
+            rua: 'Rua do alegria',
+            numImovel: '123',
+            numComodos: 15,
+          ),
+          ImovelItemCard(
+            nomeImovel: 'Casa em João Pessoa',
+            rua: 'Rua do alegria',
+            numImovel: '123',
+            numComodos: 15,
+          ),
+        ],
+      ),
+      floatingActionButton: IconeFlutuanteButton(
+        funcao: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => FormularioImovelScreen()),
+          );
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
   }
 }
