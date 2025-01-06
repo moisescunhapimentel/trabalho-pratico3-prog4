5e1f57800c212eaceed0158c6a5d279af20b5438
moisescunhapimentel <moisescunhapimentel+github@gmail.com>
2025-01-05 23:25:20 -0300

    adicionar relacionamento entre cômodo e imóvel

--- lib/data/tables/comodo_table.dart
+++ lib/data/tables/comodo_table.dart
@@ -1,10 +1,11 @@
 import 'package:drift/drift.dart';
 import 'package:trabalho3/data/enums/tipo_comodo.dart';
-import 'package:trabalho3/data/models/comodo.dart';
+import 'package:trabalho3/data/tables/imovel_table.dart';
 
-@UseRowClass(Comodo)
 class ComodoTable extends Table {
   IntColumn get id => integer().autoIncrement()();
   TextColumn get tipoComodo => textEnum<TipoComodo>()();
   IntColumn get quantidade => integer()();
+
+  IntColumn get imovelId => integer().references(ImovelTable, #id)();
 }
