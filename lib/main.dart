627481a55f74cb91c254d073fa7d955d7500345f
moisescunhapimentel <moisescunhapimentel+github@gmail.com>
2025-01-06 14:10:30 -0300

    adicionar themas no main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:trabalho3/data/dao/theme_dao.dart';
+import 'package:trabalho3/data/nosql_banco_dados.dart';
 import 'package:trabalho3/ui/models/imovel_model.dart';
 import 'package:trabalho3/ui/theme/app_theme.dart';
 import 'package:trabalho3/ui/widgets/barra_navegacao_widget.dart';
@@ -8,14 +10,19 @@ import 'package:trabalho3/ui/models/formulario_cliente_model.dart';
 import 'package:trabalho3/ui/models/formulario_contrato_model.dart';
 import 'package:trabalho3/data/banco_dados.dart';
 
+void main() async {
+  final nosql = NosqlBancoDados();
+  await nosql.init();
 
-void main() {
   runApp(
     MultiProvider(
       providers: [
         Provider<BancoDados>(create: (_) => BancoDados()),
+        Provider<NosqlBancoDados>(create: (_) => nosql),
+        Provider<ThemeDao>(
+            create: (context) => ThemeDao(Provider.of(context, listen: false))),
         ChangeNotifierProvider(
-          create: (context) => FormularioClienteModel(context.read<BancoDados>()),
+          create: (context) => FormularioClienteModel(),
         ),
         ChangeNotifierProvider(create: (context) => ImovelModel()),
         ChangeNotifierProvider(create: (context) => FormularioImovelModel()),
@@ -33,22 +40,18 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  bool isDarkMode = false;
-  void toggleTheme() {
-    setState(() {
-      isDarkMode = !isDarkMode;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: isDarkMode ? AppTheme.darkTheme : AppTheme.lightTheme,
+      theme: MediaQuery.of(context).platformBrightness == Brightness.dark
+          ? AppTheme.darkTheme.theme
+          : AppTheme.lightTheme.theme,
       home: Builder(
         builder: (context) {
           return BarraNavegacaoWidget(
-            toggleTheme: toggleTheme,
+            toggleTheme:
+                Provider.of<ThemeDao>(context, listen: false).toggleTheme,
           );
         },
       ),
