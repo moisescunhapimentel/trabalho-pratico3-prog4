506298eda130bc2b7da7ef82a59962d49c366ea1
Julianafrego <julianafgomes@aluno.uespi.br>
2025-01-06 12:12:23 -0300

    corrigir importacoes em main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:trabalho3/ui/models/imovel_model.dart';
-import 'package:trabalho3/ui/screens/inicio_screen.dart';
 import 'package:trabalho3/ui/theme/app_theme.dart';
-import 'package:trabalho3/ui/widgets/barra_navegação_widget.dart';
-import 'package:trabalho3/ui/models/navegacao_item_model.dart';
-import 'package:trabalho3/ui/models/formulario_imovel.dart';
+import 'package:trabalho3/ui/widgets/barra_navegacao_widget.dart';
+import 'package:trabalho3/ui/models/formulario_imovel_model.dart';
+import 'package:trabalho3/ui/models/formulario_cliente_model.dart';
+import 'package:trabalho3/ui/models/formulario_contrato_model.dart';
+import 'package:trabalho3/data/banco_dados.dart';
+
 
 void main() {
   runApp(
     MultiProvider(
       providers: [
+        Provider<BancoDados>(create: (_) => BancoDados()),
+        ChangeNotifierProvider(
+          create: (context) => FormularioClienteModel(context.read<BancoDados>()),
+        ),
         ChangeNotifierProvider(create: (context) => ImovelModel()),
         ChangeNotifierProvider(create: (context) => FormularioImovelModel()),
+        ChangeNotifierProvider(create: (context) => FormularioContratoModel()),
       ],
       child: MyApp(),
     ),
@@ -38,10 +45,10 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: isDarkMode ? AppTheme.darkTheme : AppTheme.lightTheme,
-      home:Builder(
+      home: Builder(
         builder: (context) {
           return BarraNavegacaoWidget(
-            toggleTheme: toggleTheme, 
+            toggleTheme: toggleTheme,
           );
         },
       ),
