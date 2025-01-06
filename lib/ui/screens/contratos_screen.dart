b881e64d0d58587e06c47b80e2cf636f1aea12c2
Julianafrego <julianafgomes@aluno.uespi.br>
2025-01-06 01:10:51 -0300

    modificar ContratosScreen

--- lib/ui/screens/contratos_screen.dart
+++ lib/ui/screens/contratos_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:trabalho3/ui/screens/formulario_contrato_screen.dart';
 import 'package:trabalho3/ui/widgets/contrato_item_card.dart';
 import '../../data/enums/tipo_intervalo.dart';
 import 'package:trabalho3/ui/widgets/icone_flutuante_button.dart';
+import 'package:trabalho3/ui/screens/contrato_screen.dart';
+
 class ContratosScreen extends StatelessWidget {
   const ContratosScreen({super.key});
 
@@ -20,12 +23,30 @@ class ContratosScreen extends StatelessWidget {
           tipoIntervalo: TipoIntervalo.anual, 
           diaPagamento: 02,
           dataVencimento: DateTime(2024, 12, 31),
-          funcao:() => {},
+          funcao: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ContratoScreen(
+                dataInicio: DateTime(2024, 1, 1),
+                dataFim: DateTime(2024, 12, 31),
+                intervaloPagamento: TipoIntervalo.anual,
+                diaPagamento: 02,
+                mesPagamento: null,
+              )),
+            );
+          },
         ),
       ]),
       
     ),
-      floatingActionButton: IconeFlutuanteButton(),
+      floatingActionButton: IconeFlutuanteButton(
+        funcao: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => FormularioContratoScreen()),
+          );
+        },
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
      
