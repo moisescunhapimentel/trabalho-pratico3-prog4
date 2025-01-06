94f29299d9d4c5bb39d64dad4bee3f74a457d578
moisescunhapimentel <moisescunhapimentel+github@gmail.com>
2025-01-05 23:24:11 -0300

    adicionar relacionamento entre contrato, cliente, pagamento e imóvel

--- lib/data/dao/contrato_dao.g.dart
+++ lib/data/dao/contrato_dao.g.dart
@@ -4,5 +4,9 @@ part of 'contrato_dao.dart';
 
 // ignore_for_file: type=lint
 mixin _$ContratoDaoMixin on DatabaseAccessor<BancoDados> {
+  $ClienteTableTable get clienteTable => attachedDatabase.clienteTable;
+  $EnderecoTableTable get enderecoTable => attachedDatabase.enderecoTable;
+  $ImovelTableTable get imovelTable => attachedDatabase.imovelTable;
   $ContratoTableTable get contratoTable => attachedDatabase.contratoTable;
+  $PagamentoTableTable get pagamentoTable => attachedDatabase.pagamentoTable;
 }
