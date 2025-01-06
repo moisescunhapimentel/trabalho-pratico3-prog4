dc5dcdea70731802cc2950582f4651a4ecd9fe3a
moisescunhapimentel <moisescunhapimentel+github@gmail.com>
2025-01-06 16:41:42 -0300

    adicionar riverpod no runApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:trabalho3/data/dao/theme_dao.dart';
-import 'package:trabalho3/data/nosql_banco_dados.dart';
-import 'package:trabalho3/ui/models/imovel_model.dart';
-import 'package:trabalho3/ui/models/theme_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:trabalho3/ui/providers/providers.dart';
 import 'package:trabalho3/ui/theme/app_theme.dart';
 import 'package:trabalho3/ui/widgets/barra_navegacao_widget.dart';
-import 'package:trabalho3/ui/models/formulario_imovel_model.dart';
-import 'package:trabalho3/ui/models/formulario_contrato_model.dart';
-import 'package:trabalho3/data/banco_dados.dart';
 
 void main() async {
-  final nosql = NosqlBancoDados();
-  final themeDao = ThemeDao(nosql);
-
-  await nosql.init();
-
   runApp(
-    MultiProvider(
-      providers: [
-        Provider<BancoDados>(
-          create: (_) => BancoDados(),
-        ),
-        Provider<ThemeDao>(
-          create: (_) => themeDao,
-        ),
-        ChangeNotifierProvider<ThemeModel>(
-          create: (_) => ThemeModel(customTheme: themeDao.getTheme()),
-        ),
-        ChangeNotifierProvider(create: (context) => ImovelModel()),
-        ChangeNotifierProvider(create: (context) => FormularioImovelModel()),
-        ChangeNotifierProvider(create: (context) => FormularioContratoModel()),
-      ],
+    const ProviderScope(
       child: MyApp(),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    final theme = Provider.of<ThemeModel>(context, listen: true);
-    final themeDao = Provider.of<ThemeDao>(context, listen: false);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(themeModelProvider);
+    final themeDao = ref.read(themeDaoProvider);
 
     return MaterialApp(
       title: 'Flutter Demo',
