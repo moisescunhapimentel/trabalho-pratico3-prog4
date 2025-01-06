309996badccb6f34406c7f8be50de1e66daeff08
Julianafrego <julianafgomes@aluno.uespi.br>
2025-01-06 01:09:33 -0300

    adicionar modelo deformulario contrato

---
+++ lib/ui/models/formulario_contrato_model.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:trabalho3/data/enums/tipo_intervalo.dart';
+import 'package:trabalho3/data/models/imovel.dart';
+import 'package:trabalho3/data/models/cliente.dart';
+
+class FormularioContratoModel with ChangeNotifier {
+  final TextEditingController dataInicioController = TextEditingController();
+  final TextEditingController dataFimController = TextEditingController();
+  final TextEditingController diaPagamentoController = TextEditingController();
+  final TextEditingController mesPagamentoController = TextEditingController();
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  TipoIntervalo _intervaloPagamento = TipoIntervalo.mensal;
+  TipoIntervalo get intervaloPagamento => _intervaloPagamento;
+
+  void updateIntervaloPagamento(TipoIntervalo intervalo) {
+    _intervaloPagamento = intervalo;
+    notifyListeners();
+  }
+
+  void selecionarDataInicio(BuildContext context) async {
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2100),
+    );
+
+    if (pickedDate != null) {
+      dataInicioController.text = '${pickedDate.day}-${pickedDate.month}-${pickedDate.year}';
+      notifyListeners();
+    }
+  }
+
+  void selecionarDataFim(BuildContext context) async {
+    DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2100),
+    );
+
+    if (pickedDate != null) {
+      dataFimController.text = '${pickedDate.day}-${pickedDate.month}-${pickedDate.year}';
+      notifyListeners();
+    }
+  }
+
+  @override
+  void dispose() {
+    dataInicioController.dispose();
+    dataFimController.dispose();
+    diaPagamentoController.dispose();
+    mesPagamentoController.dispose();
+    super.dispose();
+  }
+}
